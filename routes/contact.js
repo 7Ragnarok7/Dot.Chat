714d670ba14a47906ddf84890ac16605a204ab17
Liza Rozario <liza.rozario0809@gmail.com>
2020-08-19 13:25:02 +0530

    path defination

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Get contact page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login Page'});
+  res.render('contact', {title: 'Contact Page'});
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET registration page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login Page'});
+  res.render('register', {title: 'Signup Page'});
 });
 
 module.exports = router;
