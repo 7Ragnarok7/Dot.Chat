0d93fd90016f4ff07ad1a91216f614bd42b6f8df
Anurag Mondal <anorak318@gmail.com>
2020-09-25 14:49:43 +0530

    Updates

--- controllers/register.js
+++ controllers/register.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET registration page */
 router.get('/', function (req, res, next) {
   res.render('register', { title: 'Register', style: 'css/style.css' });
-<<<<<<< HEAD
 });
 
 router.post('/', function(req, res, next) {
@@ -12,8 +11,6 @@ router.post('/', function(req, res, next) {
       jobRoleId = req.body.job_role_id,
       userEmail = req.body.user_email,
       userPass = req.body.user_pass;
-=======
->>>>>>> 520fd320d8b42beed9344066521cb0f2c2b68d5e
 });
 
 module.exports = router;
